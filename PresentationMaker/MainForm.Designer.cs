3d0543e2388763b31a043bca489912e23b86f13f
Kirill Sokirka <sokirka2003@gmil.com>
2021-05-12 11:46:16 +0300

    Adding FileErrorProvider

--- PresentationMaker/MainForm.Designer.cs
+++ PresentationMaker/MainForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
 namespace PresentationMaker
 {
     partial class MainForm
@@ -29,15 +32,20 @@ namespace PresentationMaker
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.TextFile = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.KeyWords = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.keyWordsTip = new System.Windows.Forms.ToolTip();
+            this.ResultFile = new System.Windows.Forms.TextBox();
+            this.keyWordsTip = new System.Windows.Forms.ToolTip(this.components);
+            this.openTxtFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.GetTxtFileButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
             // label1
+            // 
             this.label1.BackColor = System.Drawing.SystemColors.Control;
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Location = new System.Drawing.Point(63, 97);
@@ -45,12 +53,17 @@ namespace PresentationMaker
             this.label1.Size = new System.Drawing.Size(250, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Вкажіть шлях до текстового файлу";
+            // 
             // TextFile
+            // 
             this.TextFile.Location = new System.Drawing.Point(63, 120);
             this.TextFile.Name = "TextFile";
             this.TextFile.Size = new System.Drawing.Size(250, 27);
             this.TextFile.TabIndex = 1;
+            this.TextFile.Validated += new System.EventHandler(this.txtFileValidate);
+            // 
             // label2
+            // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.Control;
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
@@ -60,15 +73,17 @@ namespace PresentationMaker
             this.label2.TabIndex = 2;
             this.label2.Text = "Введіть ключові слова";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // keyWordsTip
-            this.keyWordsTip.ShowAlways = true;
-            this.keyWordsTip.SetToolTip(this.KeyWords, "Формат: слово, слово");
+            // 
             // KeyWords
+            // 
             this.KeyWords.Location = new System.Drawing.Point(63, 183);
             this.KeyWords.Name = "KeyWords";
             this.KeyWords.Size = new System.Drawing.Size(250, 27);
             this.KeyWords.TabIndex = 3;
+            this.keyWordsTip.SetToolTip(this.KeyWords, "Формат: слово, слово");
+            // 
             // label3
+            // 
             this.label3.BackColor = System.Drawing.SystemColors.Control;
             this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label3.Location = new System.Drawing.Point(63, 224);
@@ -76,40 +91,72 @@ namespace PresentationMaker
             this.label3.Size = new System.Drawing.Size(250, 21);
             this.label3.TabIndex = 4;
             this.label3.Text = "Вкажіть шлях до вихідного файлу\r\n";
-            // textBox2
-            this.textBox2.Location = new System.Drawing.Point(63, 248);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(250, 27);
-            this.textBox2.TabIndex = 5;
+            // 
+            // ResultFile
+            // 
+            this.ResultFile.Location = new System.Drawing.Point(63, 248);
+            this.ResultFile.Name = "ResultFile";
+            this.ResultFile.Size = new System.Drawing.Size(250, 27);
+            this.ResultFile.TabIndex = 5;
+            // 
+            // keyWordsTip
+            // 
+            this.keyWordsTip.ShowAlways = true;
+            // 
+            // TxtFileDialog
+            // 
+            this.openTxtFileDialog.FileName = "Текстовий файл";
+            // 
+            // GetTxtFileButton
+            // 
+            this.GetTxtFileButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.GetTxtFileButton.Location = new System.Drawing.Point(319, 120);
+            this.GetTxtFileButton.Name = "GetTxtFileButton";
+            this.GetTxtFileButton.Size = new System.Drawing.Size(76, 27);
+            this.GetTxtFileButton.TabIndex = 6;
+            this.GetTxtFileButton.Text = "Browse\r\n";
+            this.GetTxtFileButton.UseVisualStyleBackColor = true;
+            this.GetTxtFileButton.Click += new System.EventHandler(GetTXTFile);
+            //
+            this.txtFileErrorProvider = new System.Windows.Forms.ErrorProvider();
+            this.txtFileErrorProvider.SetIconAlignment(this.TextFile, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
+            this.txtFileErrorProvider.SetIconPadding(this.TextFile, 2);
+            this.txtFileErrorProvider.BlinkRate = 1000;
+            this.txtFileErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.AlwaysBlink;
+            // 
             // MainForm
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(407, 388);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.GetTxtFileButton);
+            this.Controls.Add(this.ResultFile);
             this.Controls.Add(this.KeyWords);
+            this.Controls.Add(this.TextFile);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.TextFile);
             this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
             this.Name = "MainForm";
             this.Text = "PresentationMaker";
-            this.Load += new System.EventHandler(this.MainFormLoad);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
-
         #endregion
-
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox TextFile;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox KeyWords;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox ResultFile;
         private System.Windows.Forms.ToolTip keyWordsTip;
+        private System.Windows.Forms.OpenFileDialog openTxtFileDialog;
+        private System.Windows.Forms.Button GetTxtFileButton;
+        private System.Windows.Forms.ErrorProvider txtFileErrorProvider;
+        private System.Windows.Forms.ErrorProvider keywordsErrorProvider;
+        private System.Windows.Forms.ErrorProvider resultFileErrorProvider;
+        private List<bool> fieldsStatus = new List<bool>();
     }
 }
 
--- PresentationMaker/MainForm.cs
+++ PresentationMaker/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,8 +17,42 @@ namespace PresentationMaker
         {
             InitializeComponent();
         }
-        private void MainFormLoad (object sender, EventArgs e)
+        private void GetTXTFile(object sender, EventArgs e)
         {
+            openTxtFileDialog.Filter = "Текстові файли(*.txt)|*.txt|Всі файли(*.*)|*.*";
+            if (openTxtFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            this.TextFile.Text = openTxtFileDialog.FileName;
+        }
+        private void txtFileValidate(object sender, EventArgs e)
+        {
+            fieldsStatus.Add(false);
+            if (String.IsNullOrEmpty(this.TextFile.Text))
+            {
+                txtFileErrorProvider.SetError(this.TextFile, "Поле пусте");
+            }
+            else if (!IsFileValid())
+            {
+                txtFileErrorProvider.SetError(this.TextFile, "Такого файла не існує");
+            }
+            else
+            {
+                txtFileErrorProvider.SetError(this.TextFile, String.Empty);
+                fieldsStatus[0] = true;
+            }
+        }
+        private bool IsFileValid()
+        {
+            if (!CheckFiles(this.TextFile.Text))
+                return false;
+            return true;
+        }
+        private bool CheckFiles(string file)
+        {
+            FileInfo tester = new FileInfo(file);
+            if (tester.Exists)
+                return true;
+            return false;
         }
     }
 }
