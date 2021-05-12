618a5aa3853f56dd40be2af57aa86d506ecf0321
Kirill Sokirka <sokirka2003@gmil.com>
2021-05-12 18:54:09 +0300

    Adding checkinng to all fields

--- PresentationMaker/MainForm.Designer.cs
+++ PresentationMaker/MainForm.Designer.cs
@@ -41,7 +41,16 @@ namespace PresentationMaker
             this.ResultFile = new System.Windows.Forms.TextBox();
             this.keyWordsTip = new System.Windows.Forms.ToolTip(this.components);
             this.openTxtFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openResultFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.GetTxtFileButton = new System.Windows.Forms.Button();
+            this.keywordsErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtFileErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.resultFileErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.ResultFileButton = new System.Windows.Forms.Button();
+            this.MakePres = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.keywordsErrorProvider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFileErrorProvider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultFileErrorProvider)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -56,11 +65,13 @@ namespace PresentationMaker
             // 
             // TextFile
             // 
+            this.txtFileErrorProvider.SetIconAlignment(this.TextFile, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
+            this.txtFileErrorProvider.SetIconPadding(this.TextFile, 2);
             this.TextFile.Location = new System.Drawing.Point(63, 120);
             this.TextFile.Name = "TextFile";
             this.TextFile.Size = new System.Drawing.Size(250, 27);
             this.TextFile.TabIndex = 1;
-            this.TextFile.Validated += new System.EventHandler(this.txtFileValidate);
+            this.TextFile.TextChanged += new System.EventHandler(this.txtFileValidation);
             // 
             // label2
             // 
@@ -76,11 +87,15 @@ namespace PresentationMaker
             // 
             // KeyWords
             // 
+            this.keywordsErrorProvider.SetIconAlignment(this.KeyWords, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
+            this.keywordsErrorProvider.SetIconPadding(this.KeyWords, 2);
             this.KeyWords.Location = new System.Drawing.Point(63, 183);
             this.KeyWords.Name = "KeyWords";
             this.KeyWords.Size = new System.Drawing.Size(250, 27);
             this.KeyWords.TabIndex = 3;
             this.keyWordsTip.SetToolTip(this.KeyWords, "Формат: слово, слово");
+            this.KeyWords.UseWaitCursor = true;
+            this.KeyWords.TextChanged += new System.EventHandler(this.keyWordsValidation);
             // 
             // label3
             // 
@@ -94,16 +109,19 @@ namespace PresentationMaker
             // 
             // ResultFile
             // 
+            this.resultFileErrorProvider.SetIconAlignment(this.ResultFile, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
+            this.resultFileErrorProvider.SetIconPadding(this.ResultFile, 2);
             this.ResultFile.Location = new System.Drawing.Point(63, 248);
             this.ResultFile.Name = "ResultFile";
             this.ResultFile.Size = new System.Drawing.Size(250, 27);
             this.ResultFile.TabIndex = 5;
+            this.ResultFile.TextChanged += new System.EventHandler(this.resultFileValidation);
             // 
             // keyWordsTip
             // 
             this.keyWordsTip.ShowAlways = true;
             // 
-            // TxtFileDialog
+            // openTxtFileDialog
             // 
             this.openTxtFileDialog.FileName = "Текстовий файл";
             // 
@@ -116,13 +134,48 @@ namespace PresentationMaker
             this.GetTxtFileButton.TabIndex = 6;
             this.GetTxtFileButton.Text = "Browse\r\n";
             this.GetTxtFileButton.UseVisualStyleBackColor = true;
-            this.GetTxtFileButton.Click += new System.EventHandler(GetTXTFile);
-            //
-            this.txtFileErrorProvider = new System.Windows.Forms.ErrorProvider();
-            this.txtFileErrorProvider.SetIconAlignment(this.TextFile, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
-            this.txtFileErrorProvider.SetIconPadding(this.TextFile, 2);
+            this.GetTxtFileButton.Click += new System.EventHandler(this.GetTXTFile);
+            // 
+            // keywordsErrorProvider
+            // 
+            this.keywordsErrorProvider.BlinkRate = 1000;
+            this.keywordsErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.AlwaysBlink;
+            this.keywordsErrorProvider.ContainerControl = this;
+            // 
+            // txtFileErrorProvider
+            // 
             this.txtFileErrorProvider.BlinkRate = 1000;
             this.txtFileErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.AlwaysBlink;
+            this.txtFileErrorProvider.ContainerControl = this;
+            // 
+            // resultFileErrorProvider
+            // 
+            this.resultFileErrorProvider.BlinkRate = 1000;
+            this.resultFileErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.AlwaysBlink;
+            this.resultFileErrorProvider.ContainerControl = this;
+            // 
+            // ResultFileButton
+            // 
+            this.ResultFileButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ResultFileButton.Location = new System.Drawing.Point(319, 248);
+            this.ResultFileButton.Name = "ResultFileButton";
+            this.ResultFileButton.Size = new System.Drawing.Size(76, 27);
+            this.ResultFileButton.TabIndex = 7;
+            this.ResultFileButton.Text = "Browse\r\n";
+            this.ResultFileButton.UseVisualStyleBackColor = true;
+            this.ResultFileButton.Click += new System.EventHandler(this.GetResultFile);
+            // 
+            // MakePres
+            // 
+            this.MakePres.AutoSize = true;
+            this.MakePres.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MakePres.Location = new System.Drawing.Point(104, 298);
+            this.MakePres.Name = "MakePres";
+            this.MakePres.Size = new System.Drawing.Size(177, 30);
+            this.MakePres.TabIndex = 8;
+            this.MakePres.Text = "Створити презентацію\r\n";
+            this.MakePres.UseVisualStyleBackColor = true;
+            this.MakePres.Click += new System.EventHandler(this.MakePresButton);
             // 
             // MainForm
             // 
@@ -130,6 +183,8 @@ namespace PresentationMaker
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(407, 388);
+            this.Controls.Add(this.MakePres);
+            this.Controls.Add(this.ResultFileButton);
             this.Controls.Add(this.GetTxtFileButton);
             this.Controls.Add(this.ResultFile);
             this.Controls.Add(this.KeyWords);
@@ -139,11 +194,19 @@ namespace PresentationMaker
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PresentationMaker";
+            ((System.ComponentModel.ISupportInitialize)(this.keywordsErrorProvider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFileErrorProvider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultFileErrorProvider)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
         #endregion
+        private string textFile;
+        private string keyWords;
+        private string resultFile;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox TextFile;
         private System.Windows.Forms.Label label2;
@@ -152,11 +215,14 @@ namespace PresentationMaker
         private System.Windows.Forms.TextBox ResultFile;
         private System.Windows.Forms.ToolTip keyWordsTip;
         private System.Windows.Forms.OpenFileDialog openTxtFileDialog;
+        private System.Windows.Forms.OpenFileDialog openResultFileDialog;
         private System.Windows.Forms.Button GetTxtFileButton;
         private System.Windows.Forms.ErrorProvider txtFileErrorProvider;
         private System.Windows.Forms.ErrorProvider keywordsErrorProvider;
         private System.Windows.Forms.ErrorProvider resultFileErrorProvider;
-        private List<bool> fieldsStatus = new List<bool>();
+        private bool[] fieldsStatus = new bool[] { false, false, false};
+        private System.Windows.Forms.Button ResultFileButton;
+        private System.Windows.Forms.Button MakePres;
     }
 }
 
--- PresentationMaker/MainForm.cs
+++ PresentationMaker/MainForm.cs
@@ -22,30 +22,60 @@ namespace PresentationMaker
             openTxtFileDialog.Filter = "Текстові файли(*.txt)|*.txt|Всі файли(*.*)|*.*";
             if (openTxtFileDialog.ShowDialog() == DialogResult.Cancel)
                 return;
-            this.TextFile.Text = openTxtFileDialog.FileName;
+            TextFile.Text = openTxtFileDialog.FileName;
         }
-        private void txtFileValidate(object sender, EventArgs e)
+        private void GetResultFile(object sender, EventArgs e)
         {
-            fieldsStatus.Add(false);
+            openResultFileDialog.Filter = "Файли презентацій(*.pptx)|*.pptx|Всі файли(*.*)|*.*";
+            if (openResultFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            ResultFile.Text = openResultFileDialog.FileName;
+        }
+        private void keyWordsValidation(object sender, EventArgs e)
+        {
+            fieldsStatus[0] = false;
+            if (String.IsNullOrEmpty(this.KeyWords.Text))
+            {
+                keywordsErrorProvider.SetError(this.KeyWords, "Поле пусте");
+                return;
+            }
+            keywordsErrorProvider.SetError(this.KeyWords, String.Empty);
+            keyWords = KeyWords.Text;
+            fieldsStatus[0] = true;
+        }
+        private void txtFileValidation(object sender, EventArgs e)
+        {
+            fieldsStatus[1] = false;
             if (String.IsNullOrEmpty(this.TextFile.Text))
             {
                 txtFileErrorProvider.SetError(this.TextFile, "Поле пусте");
+                return;
             }
-            else if (!IsFileValid())
+            if (!CheckFiles(this.TextFile.Text))
             {
                 txtFileErrorProvider.SetError(this.TextFile, "Такого файла не існує");
+                return;
             }
-            else
-            {
-                txtFileErrorProvider.SetError(this.TextFile, String.Empty);
-                fieldsStatus[0] = true;
-            }
+            txtFileErrorProvider.SetError(this.TextFile, String.Empty);
+            textFile = TextFile.Text;
+            fieldsStatus[1] = true;
         }
-        private bool IsFileValid()
+        private void resultFileValidation(object sender, EventArgs e)
         {
-            if (!CheckFiles(this.TextFile.Text))
-                return false;
-            return true;
+            fieldsStatus[2] = false;
+            if (String.IsNullOrEmpty(this.ResultFile.Text))
+            {
+                resultFileErrorProvider.SetError(this.ResultFile, "Поле пусте");
+                return;
+            }
+            if (!CheckFiles(this.ResultFile.Text))
+            {
+                resultFileErrorProvider.SetError(this.ResultFile, "Такого файла не існує");
+                return;
+            }
+            fieldsStatus[2] = true;
+            resultFileErrorProvider.SetError(this.ResultFile, String.Empty);
+            resultFile = ResultFile.Text;
         }
         private bool CheckFiles(string file)
         {
@@ -53,6 +83,24 @@ namespace PresentationMaker
             if (tester.Exists)
                 return true;
             return false;
+        }
+        private bool CheckAllFields()
+        {
+            foreach (var item in fieldsStatus)
+            {
+                if (item == false)
+                    return false;
+            }
+            return true;
+        }
+        private void MakePresButton(object sender, EventArgs e)
+        {
+            if (!CheckAllFields())
+            {
+                MessageBox.Show("Перевірте всі поля");
+                return;
+            }
+            MessageBox.Show("Презентацію створено");
         }
     }
 }
